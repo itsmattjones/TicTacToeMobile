a0e5e88d58c79d445a516326aaffb43405ed3a06
Matt <mattjrx@gmail.com>
2020-05-12 20:24:23 +0100

    Adds additional functionality on master

--- TicTacToe/Models/Player.cs
+++ TicTacToe/Models/Player.cs
@@ -4,11 +4,13 @@
     {
         public int PlayerId { get; set; }
 
-        public bool IsPlayerTurn { get; set; }
-
         public PlayerType PlayerType { get; set; } = PlayerType.Normal;
 
+        public string PlayerName { get; set; } = "User";
+
         public int PlayerAvatar { get; set; }
+
+        public bool IsPlayerTurn { get; set; }
 
         public int TurnsTaken { get; set; }
     }
